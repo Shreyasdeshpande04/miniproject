ff2339c30020cbd4e38565b0211f33b8abbd6165
Shreyas Deshpande <deshpandeshreyas434@gmail.com>
2025-07-24 15:01:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
 import session from "express-session";
 import { userRouter } from "./Router/user.router.js";
 
@@ -10,13 +11,17 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(
-  session({
-    secret: "yourSecretKey",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+
+app.use(session({
+  secret: "your-secret-key",
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: "mongodb+srv://deshpandeshreyas434:shreyas123@cluster0.xmhcnwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    collectionName: "sessions"
+  }),
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
+}));
 
 // View engine
 app.set("view engine", "ejs");
@@ -26,8 +31,8 @@ app.use(userRouter);
 
 // MongoDB connection and server start
 mongoose
-.connect("mongodb+srv://deshpandeshreyas434:shreyas123@cluster0.xmhcnwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  
+  // .connect("mongodb://localhost:27017/project")
+  .connect("mongodb+srv://deshpandeshreyas434:shreyas123@cluster0.xmhcnwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("✅ MongoDB Connected Successfully");
     app.listen(port, () => {
